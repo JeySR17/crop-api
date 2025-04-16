945916631a5a04712dfa8aae75844b1ed311151a
JeySR17 <jeysr1763@gmail.com>
2025-04-16 16:33:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the crop recommendation model
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return 'Crop Recommendation API is running!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = [
+        data['N'],
+        data['P'],
+        data['K'],
+        data['temperature'],
+        data['humidity'],
+        data['ph'],
+        data['rainfall']
+    ]
+    prediction = model.predict([features])[0]
+    return jsonify(prediction)
