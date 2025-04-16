ecc470f3675852468854cd469efee3572c0f5469
JeySR17 <jeysr1763@gmail.com>
2025-04-16 17:00:23 +0530

    Update 2 app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -25,3 +26,8 @@ def predict():
     ]
     prediction = model.predict([features])[0]
     return jsonify(prediction)
+
+if __name__ == '__main__':
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
